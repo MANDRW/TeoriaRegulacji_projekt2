db06c678adc338575e048d62cbec6bab627a89d1
MANDRW <mandrzejewski132@gmail.com>
2024-06-13 02:27:08 +0200

    final exercise 1.1

--- z1.m
+++ z1.m
@@ -1,11 +1,8 @@
-% Parametry transmitancji
-A = 4;
+A = 4; % parametry G1(s)
 B = 1;
 
-% Definicja transmitancji
-G1 = tf(A, [B 1]);
+G1 = tf(A, [B 1]); % definicja G1(s)
 
-% Charakterystyka Nyquista
-nyquist(G1);
+nyquist(G1);% charakterystyka amplitudowo-fazowa
 title('Charakterystyka Nyquista dla G1(s) = 4 / (s + 1)');
-grid on;
+
